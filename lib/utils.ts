bfbd9dbf6e9549bee926c0725fe47ed756830a11
archerzou <archer.zou84@gmail.com>
2024-07-10 00:24:51 +0800

    add absolute path

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,15 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+
+export function absoluteUrl(path: string) {
+  if (typeof window !== "undefined") return path;
+
+  if (process.env.NEXT_PUBLIC_APP_URL)
+    return `https://${process.env.NEXT_PUBLIC_APP_URL}${path}`;
+
+  return `http://localhost:${process.env.PORT ?? 3000}${path}`;
 }
