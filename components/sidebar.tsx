5ffc6ecca4cd3aade5084ec2b487f23f113e793d
archerzou <archer.zou84@gmail.com>
2024-07-16 23:40:23 +0800

    add screenshot

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -1,21 +1,25 @@
 import Link from "next/link";
 import Image from "next/image";
-import { ClerkLoading, ClerkLoaded, UserButton } from "@clerk/nextjs";
-import { Loader } from "lucide-react";
+import { currentUser } from "@clerk/nextjs/server";
+import { ClerkLoading, ClerkLoaded, UserButton, SignedIn } from "@clerk/nextjs";
+import { Separator } from "@/components/ui/separator";
+import { Button } from "@/components/ui/button";
 
 import { cn } from "@/lib/utils";
 
 import { SidebarItem } from "./sidebar-item";
+import RepoStar from "./repo-star";
 
 type Props = {
   className?: string;
 };
 
-export const Sidebar = ({ className }: Props) => {
+export const Sidebar = async ({ className }: Props) => {
+  const user = await currentUser();
   return (
     <div
       className={cn(
-        "flex h-full lg:w-[256px] lg:fixed left-0 top-0 px-4 border-r-2 flex-col",
+        "left-0 top-0 flex flex-col h-full border-r-2 px-4 md:fixed md:w-[225px] lg:w-[256px]",
         className
       )}
     >
@@ -37,13 +41,60 @@ export const Sidebar = ({ className }: Props) => {
         <SidebarItem label="quests" href="/quests" iconSrc="/quests.svg" />
         <SidebarItem label="shop" href="/shop" iconSrc="/shop.svg" />
       </div>
-      <div className="p-4">
-        <ClerkLoading>
-          <Loader className="h-5 w-5 text-muted-foreground animate-spin" />
-        </ClerkLoading>
-        <ClerkLoaded>
-          <UserButton afterSignOutUrl="/" />
-        </ClerkLoaded>
+
+      <Separator className="h-0.5" />
+
+      <div className="flex flex-col gap-y-4 mt-4">
+        <div className="flex items-center justify-center gap-x-2">
+          <RepoStar />
+
+          <span className="text-sm font-bold uppercase w-full p-2">
+            Star on GitHub
+          </span>
+        </div>
+
+        <div className="flex items-center justify-center gap-x-2 mb-4">
+          <ClerkLoading>
+            <SignedIn>
+              <Button
+                disabled
+                size="rounded"
+                className="h-[40px] w-[40px] animate-pulse bg-gray-200 ring ring-border"
+              />
+
+              <div className="flex flex-col h-[52px] w-[158px] gap-y-1 p-2">
+                <div className="h-16 bg-gray-200 animate-pulse rounded-xl" />
+                <div className="h-12 bg-gray-200 animate-pulse rounded-xl" />
+              </div>
+            </SignedIn>
+          </ClerkLoading>
+
+          <ClerkLoaded>
+            <SignedIn>
+              <UserButton
+                afterSignOutUrl="/"
+                appearance={{
+                  elements: {
+                    userButtonPopoverCard: {
+                      pointerEvents: "initial",
+                      width: "300px",
+                    },
+                    userButtonAvatarBox: {
+                      height: "40px",
+                      width: "40px",
+                    },
+                  },
+                }}
+              />
+
+              <div className="flex w-full">
+                <span className="text-sm font-bold">
+                  {user?.firstName || "Anon"}
+                </span>
+              </div>
+            </SignedIn>
+          </ClerkLoaded>
+        </div>
       </div>
     </div>
   );
